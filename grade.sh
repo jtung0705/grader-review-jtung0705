2f03717e648c6d48bb59a31f9f7970e437a80d2b
Jeffery Tung <120071290+jtung0705@users.noreply.github.com>
2023-11-08 15:16:42 -0800

    updated a lot

--- grade.sh
+++ grade.sh
@@ -6,11 +6,22 @@ rm -rf grading-area
 mkdir grading-area
 
 git clone $1 student-submission
-echo 'Finished cloning'
+files=`find student-submission -name "*.java"`
+for file in $files
+    do
+        if [[ -f $file && $file == **/ListExamples.java* ]]
+        then
+        echo 'Found file'
+        else
+        echo "Did not find ListExamples.java file"
+        fi
+    done
+echo "Finished cloning"
 
-
+cp student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
+cp lib grading-area 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
-
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
