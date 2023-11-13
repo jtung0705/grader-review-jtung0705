10f77508a0d1fc039b10a3430ad49a7a74eb43e1
Jeffery Tung <120071290+jtung0705@users.noreply.github.com>
2023-11-12 16:51:26 -0800

    edited

--- grade.sh
+++ grade.sh
@@ -4,8 +4,9 @@ rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
-
+mkdir student-submission
 git clone $1 student-submission
+
 files=`find student-submission -name "*.java"`
 for file in $files
     do
@@ -20,8 +21,12 @@ echo "Finished cloning"
 
 cp student-submission/ListExamples.java grading-area
 cp TestListExamples.java grading-area
-cp lib grading-area 
+cp -r lib grading-area 
+cd grading-area/
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java > h.txt
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
 # Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+# getting to this point, find out how to grep
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
